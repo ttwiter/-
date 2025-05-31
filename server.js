649631c0ef21dff75a1a4caf728cf17b98e2df75
ttwiter <mairah2810@gmail.com>
2025-05-31 14:19:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,37 @@ app.get('/iklan.json', (req, res) => {
     res.type('json').send(data);
   });
 });
+// Tambahkan di bawah endpoint iklan.json di server.js
+
+// Endpoint untuk GET slide promosi
+app.get('/slide.json', (req, res) => {
+  fs.readFile(path.join(__dirname, 'slide.json'), (err, data) => {
+    if (err) return res.json([]);
+    res.type('json').send(data);
+  });
+});
+
+// Endpoint untuk POST slide promosi
+app.post('/slide.json', (req, res) => {
+  fs.readFile(path.join(__dirname, 'slide.json'), (err, data) => {
+    let slides = [];
+    if (!err && data.length) slides = JSON.parse(data);
+    if (req.body.deleteIdx !== undefined) {
+      slides.splice(req.body.deleteIdx, 1);
+    } else {
+      slides.unshift(req.body);
+    }
+    fs.writeFile(
+      path.join(__dirname, 'slide.json'),
+      JSON.stringify(slides, null, 2),
+      err => {
+        if (err) return res.status(500).json({error: "Gagal simpan"});
+        res.json({success: true});
+      }
+    );
+  });
+});
+
 app.post('/iklan.json', (req, res) => {
   fs.readFile(path.join(__dirname, 'iklan.json'), (err, data) => {
     let iklan = [];
